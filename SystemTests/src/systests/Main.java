7f817186e460a24584d0b3d3fc2ece115dc7757d
ivasylie <rodiy@ukr.net>
2017-04-25 15:37:42 +0200

    Added jersey libraries + initial class

--- SystemTests/src/systests/Main.java
+++ SystemTests/src/systests/Main.java
@@ -1,5 +1,20 @@
 package systests;
 
-public class Main {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+public class Main
+{
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
 
 }
